206f5560a080c6889b199b350816e7e3b01649d4
ZamestoTV <34053496+Hubbotu@users.noreply.github.com>
2025-11-16 07:06:07 +0300

    Add files via upload

---
+++ localization.lua
@@ -0,0 +1,169 @@
+-- English, the default
+
+BINDING_HEADER_NOTESHEADER = "Notes";
+BINDING_NAME_NOTESTOGGLE = "Toggle notes";
+BINDING_NAME_MININOTESTOGGLE = "Toggle mini notes";
+
+NOTES_TITLE = "Notes";
+NOTES_CHANGETITLE = "Edit the note title";
+NOTES_CHANGECATEGORY = "Edit the note category";
+
+NOTES_TEMPLATE_TITLE = "New note"
+
+NOTES_NO_NOTES_TEXT = "No notes available";
+NOTES_OK = "OK";
+NOTES_CANCEL = "Cancel";
+
+NOTES_NEW = "New";
+NOTES_COPY = "Copy";
+NOTES_DELETE = "Delete";
+NOTES_ADD = "+";
+NOTES_SEND = "Send";
+
+NOTES_TYPE = "Type";
+NOTES_TYPE_NOTODO = "No todo";
+NOTES_TYPE_TODODONE = "Done";
+NOTES_TYPE_TODONOTDONE = "Not done";
+
+NOTES_CATEGORY_QUESTLINK = "Questlink"
+NOTES_CATEGORY_QUESTNAME = "Questname"
+
+NOTES_SAVE_MAIL_TOOLTIP = "Save mail as note."
+
+NOTES_HELP_OPEN_CLOSE = "open/close notes frame"
+NOTES_HELP_SHOW_HIDE = "show/hide minimap button"
+
+NOTES_DATE = "Date"
+NOTES_POINTS = "Points"
+
+-- German
+if (GetLocale() == "deDE") then
+
+    BINDING_HEADER_NOTESHEADER = "Notizen";
+    BINDING_NAME_NOTESTOGGLE = "Notizen an/ausschalten";
+    BINDING_NAME_MININOTESTOGGLE = "Mini-Notizen an/ausschalten";
+
+    NOTES_TITLE = "Notizen";
+    NOTES_CHANGETITLE = "Titel der Notiz \195\164ndern";
+    NOTES_CHANGECATEGORY = "Kategorie der Notiz \195\164ndern";
+
+    NOTES_TEMPLATE_TITLE = "Neu Notiz"
+
+    NOTES_NO_NOTES_TEXT = "Es gibt keine Notizen.";
+    NOTES_OK = "OK";
+    NOTES_CANCEL = "Abbrechen";
+
+    NOTES_NEW = "Neu";
+    NOTES_COPY = "Kopieren";
+    NOTES_DELETE = "L\195\182schen";
+    NOTES_ADD = "+";
+    NOTES_SEND = "Senden";
+
+    NOTES_TYPE = "Typ";
+    NOTES_TYPE_NOTODO = "kein Todo";
+    NOTES_TYPE_TODODONE = "fertig";
+    NOTES_TYPE_TODONOTDONE = "nicht fertig";
+
+    NOTES_CATEGORY_QUESTLINK = "Questlink"
+    NOTES_CATEGORY_QUESTNAME = "Questname"
+
+    NOTES_SAVE_MAIL_TOOLTIP = "Mail als Notiz speichern."
+
+    NOTES_HELP_OPEN_CLOSE = "Notes \195\182ffnen oder schlie\195\159en"
+    NOTES_HELP_SHOW_HIDE = "Minimap Button anzeigen oder verbergen"
+
+    NOTES_DATE = "Datum"
+    NOTES_POINTS = "Punkte"
+
+    -- French
+elseif (GetLocale() == "frFR") then
+
+    BINDING_HEADER_NOTESHEADER = "Notes";
+    BINDING_NAME_NOTESTOGGLE = "Afficher les Notes";
+
+    NOTES_TITLE = "Notes";
+    NOTES_CHANGETITLE = "Modifier le titre";
+    NOTES_CHANGECATEGORY = "Modifier la cat\195\169gorie";
+
+    NOTES_TEMPLATE_TITLE = "Nouvelle note"
+
+    NOTES_NO_NOTES_TEXT = "Pas de notes";
+    NOTES_OK = "Valider";
+    NOTES_CANCEL = "Annuler";
+
+    NOTES_NEW = "Nouvelle";
+    NOTES_COPY = "Copier";
+    NOTES_DELETE = "Supprimer";
+    NOTES_ADD = "+";
+    NOTES_SEND = "Envoyer";
+
+    NOTES_TYPE = "Type";
+    NOTES_TYPE_NOTODO = "Aucun";
+    NOTES_TYPE_TODODONE = "Termin\195\169";
+    NOTES_TYPE_TODONOTDONE = "En cours";
+
+    NOTES_CATEGORY_QUESTLINK = "Ajouter le lien vers la qu\195\170te"
+    NOTES_CATEGORY_QUESTNAME = "Ajouter le nom de la qu\195\170te"
+	
+    -- Russian (Translator ZamestoTV)
+elseif (GetLocale() == "ruRU") then
+	BINDING_HEADER_NOTESHEADER = "Заметки";
+    BINDING_NAME_NOTESTOGGLE = "Открыть/закрыть заметки";
+	NOTES_TITLE = "Заметки";
+    BINDING_NAME_MININOTESTOGGLE = "Открыть/закрыть мини-заметки";
+    NOTES_CHANGETITLE = "Редактировать заголовок заметки";
+	NOTES_CHANGECATEGORY = "Редактировать категорию заметки";
+    NOTES_TEMPLATE_TITLE = "Новая заметка"
+    NOTES_NO_NOTES_TEXT = "Нет доступных заметок";
+    NOTES_OK = "ОК";
+    NOTES_CANCEL = "Отмена";
+    NOTES_NEW = "Новая";
+    NOTES_COPY = "Копировать";
+    NOTES_DELETE = "Удалить";
+    NOTES_ADD = "+";
+    NOTES_SEND = "Отправить";
+    NOTES_TYPE = "Тип";
+    NOTES_TYPE_NOTODO = "Без задач";
+    NOTES_TYPE_TODODONE = "Выполнено";
+    NOTES_TYPE_TODONOTDONE = "Не выполнено";
+    NOTES_CATEGORY_QUESTLINK = "Ссылка на задание"
+    NOTES_CATEGORY_QUESTNAME = "Название задания"
+    NOTES_SAVE_MAIL_TOOLTIP = "Сохранить письмо как заметку."
+    NOTES_HELP_OPEN_CLOSE = "открыть/закрыть окно заметок"
+    NOTES_HELP_SHOW_HIDE = "показать/скрыть кнопку у миникарты"
+    NOTES_DATE = "Дата"
+    NOTES_POINTS = "Очки"
+
+end
+
+
+
+-- � = \195\169
+
+-- � = \195\168
+
+-- � = \195\170
+
+-- � = \195\171
+
+
+
+-- � = \195\132
+
+-- � = \195\164
+
+
+
+-- � = \195\150
+
+-- � = \195\182
+
+
+
+-- � = \195\156
+
+-- � = \195\188
+
+
+
+-- � = \195\159
